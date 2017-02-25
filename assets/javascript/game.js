de48e31b015d0d294ab00eafd2c8139ddf73c4e8
John Zoshak <JohnZoshak@Johns-MacBook-Air-2.local>
2017-02-25 16:07:37 -0500

    refactor - code now slightly DRY-er.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
 /* Some Needed Variables and Functions */
+function reset() {
+	var guesses = 15; //resets guess counter to 15
+	document.getElementById("remaining").innerHTML = guesses; //sets the HTML to show 15
+	currentWord = wordbank[Math.floor(Math.random()*wordbank.length)]; //sets a new word
+	wordBlankResults = wordblanks(currentWord); //sets the new blank length
+	wordSplit = currentWord.split(""); //splits the current word into an array
+	document.getElementById("word_spaces").innerHTML = wordBlankResults.join(" "); //adds the blanks to the page
+	guessed = []; // empties out the guessed array.
+	document.getElementById("guessed_letters").innerHTML = guessed.join(" "); //displays the emptied out array. 
+}
 
 function wordblanks (word) { //function that sets the blanks in the beginning. 
 		var blanks = [];
@@ -8,7 +18,7 @@ function wordblanks (word) { //function that sets the blanks in the beginning.
 		return blanks;
 	}
 
-	function iHasUnderScores(arr) {
+function iHasUnderScores(arr) {
 	var underscore = arr.indexOf("_ ");
 	if (underscore === -1) {
 		return false;
@@ -26,7 +36,6 @@ var currentWord = ""; //current word variable e.g. the word being used.
 var guessed = []; //empty array to hold guessed letters.
 var currentWord = wordbank[Math.floor(Math.random()*wordbank.length)];
  //Uses Math floor and Math random to pick a random item out of our wordbank array.
-
 var wordBlankResults = wordblanks(currentWord); //actually sets the blanks and holds them in a variable.
 var wordSplit = currentWord.split(""); //splits the current word into an array. 
 
@@ -36,25 +45,18 @@ document.getElementById("word_spaces").innerHTML = wordBlankResults.join(" "); /
 
 document.onkeyup = function(e) {  
 	var userInput = e.key;
-	var wrongGuessCounter = 0;
+	var wrongGuessCounter = 0; //hacky counter variable i needed to implement when comparing the arrays. 
 	
 	for (i=0; i < wordSplit.length; i++) {
-		if(userInput === wordSplit[i]) {
-			wordBlankResults[i] = userInput
-			document.getElementById("word_spaces").innerHTML = wordBlankResults.join(" ");
-			iHasUnderScores(wordBlankResults);
+		if(userInput === wordSplit[i]) { //checks the userInput against the Wordsplit array.
+			wordBlankResults[i] = userInput // adds the character to the blank array.
+			document.getElementById("word_spaces").innerHTML = wordBlankResults.join(" "); //displays the new less-blank array
+			iHasUnderScores(wordBlankResults); //checks to see if the array has any underscores left.
 			
-				if (iHasUnderScores(wordBlankResults) === false) {
-					guesses = 15;
-					document.getElementById("remaining").innerHTML = guesses;
-					currentWord = wordbank[Math.floor(Math.random()*wordbank.length)];
-					wordBlankResults = wordblanks(currentWord);
-					wordSplit = currentWord.split("");
-					document.getElementById("word_spaces").innerHTML = wordBlankResults.join(" ");
-					guessed = [];
-					document.getElementById("guessed_letters").innerHTML = guessed.join(" ");
-					wins = wins + 1;
-					document.getElementById("win_count").innerHTML = wins;
+				if (iHasUnderScores(wordBlankResults) === false) { // if it doesn't have underscores
+					reset(); // reset function
+					wins += 1; //add one to wins
+					document.getElementById("win_count").innerHTML = wins; //display new wins. 
 			}
 		}
 
@@ -65,24 +67,17 @@ document.onkeyup = function(e) {
 		}  
 
 		if (wrongGuessCounter === wordSplit.length) { //if it checks the entire array and no match, run this code.
-			guessed.push(userInput);
-			guesses = guesses - 1;
-			document.getElementById("guessed_letters").innerHTML = guessed.join(" ");
-			document.getElementById("remaining").innerHTML = guesses;
+			guessed.push(userInput); //pushes the guessed letter to the array
+			guesses -= 1; // subtracts from guesses 
+			document.getElementById("guessed_letters").innerHTML = guessed.join(" "); // displays the new array
+			document.getElementById("remaining").innerHTML = guesses; // displays the new guesses
 			
 		}
 
 	}
 
-	if (guesses === 0) {
-	guesses = 15;
-	document.getElementById("remaining").innerHTML = guesses;
-	currentWord = wordbank[Math.floor(Math.random()*wordbank.length)];
-	wordBlankResults = wordblanks(currentWord);
-	wordSplit = currentWord.split("");
-	document.getElementById("word_spaces").innerHTML = wordBlankResults.join(" ");
-	guessed = [];
-	document.getElementById("guessed_letters").innerHTML = guessed.join(" ");
+	if (guesses === 0) { //if guesses equals 0 then YOU LOSE! 
+	reset(); //runs the reset function. 
 }
 
 }
